f62e7ebbe8d13609f0fa9209fd047d1154cb1701
Changsik Yoon <dbs2636@gmail.com>
2022-01-26 22:02:52 +0900

    style: Add hover effect to PostItem Card

--- src/components/post/PostItem.tsx
+++ src/components/post/PostItem.tsx
@@ -1,8 +1,17 @@
 import { Card, Typography, CardContent, styled, Box } from "@mui/material";
+import { Link } from "gatsby";
 import { GatsbyImage } from "gatsby-plugin-image";
 import React from "react";
 import { PostNode } from "../../types/postTypes";
 import TagList from "../tag/TagList";
+
+const HoverCard = styled(Card)`
+  transition: transform 0.2s ease-in-out;
+
+  &:hover {
+    transform: scale(1.01);
+  }
+`;
 
 const Title = styled(Typography)`
   font-weight: 600;
@@ -35,25 +44,27 @@ const PostItem = ({ post }: PostItemProps) => {
   const { title, date, tags, category, thumbnail } = post.frontmatter;
 
   return (
-    <Card variant="outlined">
-      <CardFlexContent>
-        <Box>
-          <Typography variant="subtitle1" color="primary">
-            {category.toUpperCase()}
-          </Typography>
-          <Title variant="h5">{title}</Title>
-          <Typography variant="caption">{date}</Typography>
-          <Typography variant="body2" color="text.secondary">
-            {post.excerpt}
-          </Typography>
-          <TagList tags={tags} />
-        </Box>
-        <CardThumbnail
-          image={thumbnail.childImageSharp.gatsbyImageData}
-          alt={`${title} thumbnail`}
-        />
-      </CardFlexContent>
-    </Card>
+    <Link to={`blog/${post.slug}`}>
+      <HoverCard variant="outlined">
+        <CardFlexContent>
+          <Box>
+            <Typography variant="subtitle1" color="primary">
+              {category.toUpperCase()}
+            </Typography>
+            <Title variant="h5">{title}</Title>
+            <Typography variant="caption">{date}</Typography>
+            <Typography variant="body2" color="text.secondary">
+              {post.excerpt}
+            </Typography>
+            <TagList tags={tags} />
+          </Box>
+          <CardThumbnail
+            image={thumbnail.childImageSharp.gatsbyImageData}
+            alt={`${title} thumbnail`}
+          />
+        </CardFlexContent>
+      </HoverCard>
+    </Link>
   );
 };
 
