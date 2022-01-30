d9f3c1ba7908eb4e5e146337e58dc53fb768e1e9
Changsik Yoon <dbs2636@gmail.com>
2022-01-30 18:24:43 +0900

    feat: Add footer

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,7 +1,29 @@
+import styled from "@emotion/styled";
+import { Box, Container, Typography } from "@mui/material";
 import React from "react";
 
+const FooterBlock = styled.footer`
+  background-color: #303740;
+  color: #eeeeee;
+`;
+
+const FooterBox = styled(Box)`
+  padding: 10px 0;
+  text-align: center;
+`;
+
 const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <FooterBlock>
+      <Container>
+        <FooterBox>
+          <Typography variant="caption">
+            @2022 Caesiumy build with Gatsby
+          </Typography>
+        </FooterBox>
+      </Container>
+    </FooterBlock>
+  );
 };
 
 export default Footer;
