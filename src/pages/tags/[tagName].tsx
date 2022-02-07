c4c0c5588b486ca627b98bb3f688e121f280a6e2
Changsik Yoon <dbs2636@gmail.com>
2022-02-07 22:32:51 +0900

    feat: Improve TagDetailPage with layouts

--- src/pages/tags/[tagName].tsx
+++ src/pages/tags/[tagName].tsx
@@ -1,8 +1,22 @@
 import React from "react";
+import Intro from "../../components/common/Intro";
+import ContentsLayout from "../../components/layout/ContentsLayout";
+import Layout from "../../components/layout/Layout";
 
-const TagsDetailPage = (params) => {
-  console.log("params :>> ", params);
-  return <div>Tags page item {params.tagName}</div>;
+type TagDetailPageProps = {
+  tagName: string;
+};
+
+const TagsDetailPage = ({ tagName }: TagDetailPageProps) => {
+  return (
+    <Layout>
+      <Intro type="tag" selected={`#${tagName}`} />
+      <ContentsLayout>
+        {/* TODO - Add tag filtered post list */}
+        postlist
+      </ContentsLayout>
+    </Layout>
+  );
 };
 
 export default TagsDetailPage;
