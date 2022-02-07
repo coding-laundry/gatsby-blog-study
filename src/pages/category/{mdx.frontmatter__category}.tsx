cc4bb2b184f90506bb875c0c178d39e21ae1f1c1
Changsik Yoon <dbs2636@gmail.com>
2022-02-07 23:05:07 +0900

    feat: Add filtered post list in CategoryDetailPage

--- src/pages/category/{mdx.frontmatter__category}.tsx
+++ src/pages/category/{mdx.frontmatter__category}.tsx
@@ -1,27 +1,57 @@
+import { graphql } from "gatsby";
 import React from "react";
-import CategoryList from "../../components/category/CategoryList";
 import Intro from "../../components/common/Intro";
 import ContentsLayout from "../../components/layout/ContentsLayout";
 import Layout from "../../components/layout/Layout";
+import PostList from "../../components/post/PostList";
+import { AllMarkdown } from "../../types/postTypes";
 
 type CategoryDetailPageProps = {
   params: {
     frontmatter__category: string;
   };
+  data: AllMarkdown;
 };
 
-const CategoryDetailPage = ({ params }: CategoryDetailPageProps) => {
+const CategoryDetailPage = ({ params, data }: CategoryDetailPageProps) => {
   const selectedCategory = params.frontmatter__category;
-
+  const { edges: posts } = data.allMdx;
   return (
     <Layout>
       <Intro type="category" selected={selectedCategory} />
       <ContentsLayout selectedCategory={selectedCategory}>
-        {/* TODO - Add category filtered post list */}
-        postlist
+        <PostList posts={posts} />
       </ContentsLayout>
     </Layout>
   );
 };
 
 export default CategoryDetailPage;
+
+export const query = graphql`
+  query ($frontmatter__category: String) {
+    allMdx(
+      filter: { frontmatter: { category: { eq: $frontmatter__category } } }
+    ) {
+      edges {
+        node {
+          frontmatter {
+            title
+            tags
+            date
+            category
+            thumbnail {
+              childImageSharp {
+                gatsbyImageData
+              }
+              publicURL
+            }
+          }
+          slug
+          excerpt(pruneLength: 64)
+          id
+        }
+      }
+    }
+  }
+`;
