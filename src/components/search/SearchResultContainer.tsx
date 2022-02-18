b5af2f820b83d2d8c3d6e0122f212b04197dab26
Changsik Yoon <dbs2636@gmail.com>
2022-02-18 20:57:23 +0900

    feat: Add get query string

--- src/components/search/SearchResultContainer.tsx
+++ src/components/search/SearchResultContainer.tsx
@@ -1,18 +1,22 @@
 import { Box } from "@mui/material";
 import React, { useState } from "react";
 import SearchInput from "./SearchInput";
+import PostList from "../post/PostList";
+import { navigate } from "gatsby";
 
 const SearchResultContainer = () => {
   const [value, setValue] = useState("");
 
   const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    console.log("e.target.value", e.target.value);
-    setValue(e.target.value);
+    const { value } = e.target;
+    setValue(value);
+    navigate(`?q=${value}`);
   };
 
   return (
     <Box>
       <SearchInput onChange={onChangeInput} value={value} />
+      <PostList posts={[]} />
     </Box>
   );
 };
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import { PageProps } from "gatsby";
+import React, { useMemo } from "react";
 import Intro from "../../components/common/Intro";
 import ContentsLayout from "../../components/layout/ContentsLayout";
 import Layout from "../../components/layout/Layout";
 import SearchResultContainer from "../../components/search/SearchResultContainer";
 
-const SearchIndex = () => {
+interface SearchIndexProps {
+  location: Pick<PageProps, "location">;
+}
+
+const SearchIndex = ({ location }: PageProps) => {
+  const { search } = location;
+
+  const qs = useMemo(
+    () =>
+      search
+        .slice(1)
+        .split("&")
+        .reduce((acc, cur) => {
+          const [key, value] = cur.split("=");
+          acc[key] = value;
+          return acc;
+        }, {}),
+    [search]
+  );
+
+  console.log("qs :>> ", qs);
+
   return (
     <Layout>
       <Intro type="Search" selected={""} />
