489c61fd86f353cde6edb06cadd45bf7903bb3f2
Changsik Yoon <dbs2636@gmail.com>
2022-02-20 21:37:05 +0900

    feat: Add about page

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,33 @@
+import { Container } from "@mui/material";
+import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
 import React from "react";
+import Layout from "../../components/layout/Layout";
 
-const About = () => {
-  return <div>about</div>;
+interface AboutProps {
+  data: {
+    mdx: {
+      body: string;
+    };
+  };
+}
+
+const About = ({ data }: AboutProps) => {
+  return (
+    <Layout>
+      <Container component="section">
+        <MDXRenderer>{data.mdx.body}</MDXRenderer>
+      </Container>
+    </Layout>
+  );
 };
 
 export default About;
+
+export const query = graphql`
+  query {
+    mdx(fields: { source: { eq: "about" } }) {
+      body
+    }
+  }
+`;
