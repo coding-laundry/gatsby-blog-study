156a4ece6748fc09ff2aa3b580c9b2c5678b04b7
Changsik Yoon <dbs2636@gmail.com>
2022-02-25 00:05:26 +0900

    feat: Add throttle to useScroll hook & fix: Fix mdx version error

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -1,20 +1,22 @@
 import { useEffect, useState } from "react";
+import throttle from "lodash.throttle";
+
+const DELAY = 10;
 
 export const useScroll = () => {
   const [curHeight, setCurrentHeight] = useState(1);
   const [scrollProgress, setScrollProgress] = useState(1);
 
-  const setScrollConfig = () => {
+  const setScrollConfig = throttle(() => {
     setCurrentHeight(window.innerHeight + window.scrollY);
 
     const progress =
       (window.scrollY / (document.body.offsetHeight - window.innerHeight)) *
       100;
     setScrollProgress(Math.ceil(progress));
-  };
+  }, DELAY);
 
   useEffect(() => {
-    // TODO - scroll 이벤트에 throttle 적용하기
     window.addEventListener("scroll", setScrollConfig);
 
     return () => {
