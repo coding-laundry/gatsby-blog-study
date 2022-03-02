c3a8770f42e082392988945d6d7573a96144f939
Changsik Yoon <dbs2636@gmail.com>
2022-03-02 17:12:27 +0900

    style: Add HiddenBox

--- src/components/layout/ContentsLayout.tsx
+++ src/components/layout/ContentsLayout.tsx
@@ -1,3 +1,5 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Stack from "@mui/material/Stack";
@@ -6,6 +8,12 @@ import { PostNode } from "../../types/postTypes";
 import CategoryList from "../category/CategoryList";
 import TableOfContentsList from "../post/TableOfContentsList";
 import TagList from "../tag/TagList";
+
+const HiddenBox = styled(Box)`
+  @media (max-width: 600px) {
+    display: none;
+  }
+`;
 
 type ContentsLayoutProps = {
   children: React.ReactNode;
@@ -24,10 +32,12 @@ const ContentsLayout = ({
           {children}
         </Grid>
         <Grid item sm={3} xs={12} component="aside">
-          {tableOfContents && (
-            <TableOfContentsList tableOfContents={tableOfContents} />
-          )}
           <Stack spacing={2} sx={{ position: "sticky", top: 65 }}>
+            {tableOfContents && (
+              <HiddenBox>
+                <TableOfContentsList tableOfContents={tableOfContents} />
+              </HiddenBox>
+            )}
             <CategoryList selectedCategory={selectedCategory} />
             <TagList limit={25} />
           </Stack>
