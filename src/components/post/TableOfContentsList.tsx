34bf2c19c749600f5e298d3310b2b0cb6ff044e2
Changsik Yoon <dbs2636@gmail.com>
2022-03-02 17:09:36 +0900

    feat: Add list items to TableOfContentsList

--- src/components/post/TableOfContentsList.tsx
+++ src/components/post/TableOfContentsList.tsx
@@ -1,11 +1,52 @@
+import styled from "@emotion/styled";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Stack,
+} from "@mui/material";
 import React from "react";
 import { PostNode } from "../../types/postTypes";
+
+const TableStack = styled(Stack)`
+  width: 100%;
+`;
+
+const NestedList = styled(List)`
+  border-left: 1px solid lightgray;
+  margin-left: 1rem;
+`;
 
 const TableOfContentsList = ({
   tableOfContents,
 }: Pick<PostNode, "tableOfContents">) => {
-  console.log("tableOfContents", tableOfContents);
-  return <div>TableOfContentsList</div>;
+  const { items } = tableOfContents;
+  return (
+    <Box>
+      <List dense>
+        {items.map((item) => (
+          <ListItem key={item.url} disableGutters>
+            <TableStack>
+              <ListItemButton component="a" href={item.url}>
+                <ListItemText primary={item.title} sx={{ fontSize: "1rem" }} />
+              </ListItemButton>
+              <NestedList disablePadding dense>
+                {item.items.map((item) => (
+                  <ListItem key={item.url} disableGutters>
+                    <ListItemButton component="a" href={item.url}>
+                      <ListItemText primary={item.title} />
+                    </ListItemButton>
+                  </ListItem>
+                ))}
+              </NestedList>
+            </TableStack>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 };
 
 export default TableOfContentsList;
