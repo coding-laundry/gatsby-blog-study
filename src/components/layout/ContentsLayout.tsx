66a0fe622e00299a4bb573d8a3bc6fe75ca48976
Changsik Yoon <dbs2636@gmail.com>
2022-03-05 21:55:07 +0900

    fix: Add exit case for none head

--- src/components/layout/ContentsLayout.tsx
+++ src/components/layout/ContentsLayout.tsx
@@ -32,7 +32,7 @@ const ContentsLayout = ({
         </Grid>
         <Grid item sm={3} xs={12} component="aside">
           <StickyStack spacing={2}>
-            {tableOfContents && (
+            {tableOfContents?.items && (
               <TableOfContentsBox items={tableOfContents.items} />
             )}
             <CategoryList selectedCategory={selectedCategory} />
