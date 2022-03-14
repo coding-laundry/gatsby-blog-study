0bd4e32c65620dc000867c3085f24ac528b8d5a0
Changsik Yoon <dbs2636@gmail.com>
2022-03-14 22:01:42 +0900

    feat: Improve darkMode using useDarkMode

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,20 +1,16 @@
-import { Box, Container, ThemeProvider } from "@mui/material";
+import { Box, Container } from "@mui/material";
 import React from "react";
 import useDarkMode from "use-dark-mode";
-import { useCustomTheme } from "../../hooks/useCustomTheme";
 import Footer from "../common/Footer";
 import Header from "../common/Header";
 import Profile from "../common/Profile";
-import { ColorModeContext } from "./ThemeLayout";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
-  // const { theme, toggleMode } = useCustomTheme();
   const darkMode = useDarkMode();
-  const colorMode = React.useContext(ColorModeContext);
 
   return (
     <Box
@@ -23,7 +19,7 @@ const Layout = ({ children }: LayoutProps) => {
         color: "text.primary",
       }}
     >
-      <Header toggleMode={colorMode.toggleColorMode} />
+      <Header toggleMode={() => darkMode.toggle()} />
       {children}
 
       <Container component="section">
--- src/components/layout/ThemeLayout.tsx
+++ src/components/layout/ThemeLayout.tsx
@@ -1,61 +1,24 @@
-import {
-  createTheme,
-  CssBaseline,
-  ThemeProvider,
-  useTheme,
-} from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import React, { useEffect, useMemo, useState } from "react";
 import useDarkMode from "use-dark-mode";
 import { createCustomTheme } from "../../hooks/useCustomTheme";
-
-export const ColorModeContext = React.createContext({
-  toggleColorMode: () => {},
-});
-
 interface ThemeLayoutProps {
   children: React.ReactNode;
 }
 
 const ThemeLayout = ({ children }: ThemeLayoutProps) => {
   const darkMode = useDarkMode();
+  const [theme, setTheme] = useState(createCustomTheme("dark"));
 
-  const [mode, setMode] = React.useState<"light" | "dark">("light");
-  const colorMode = React.useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-      },
-    }),
-    []
-  );
-
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-        },
-      }),
-    [mode]
-  );
-
-  const theme2 = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode: darkMode.value ? "dark" : "light",
-        },
-      }),
-    [darkMode.value]
-  );
+  useEffect(() => {
+    setTheme(createCustomTheme(darkMode.value ? "dark" : "light"));
+  }, [darkMode.value]);
 
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        {children}
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
   );
 };
 
