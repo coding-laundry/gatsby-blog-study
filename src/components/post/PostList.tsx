191786f3ed61380e8692c4062b06f3bc3fd8414d
Changsik Yoon <dbs2636@gmail.com>
2022-03-16 23:35:10 +0900

    feat: Add pagination logic

--- src/components/post/PostList.tsx
+++ src/components/post/PostList.tsx
@@ -1,8 +1,12 @@
 import styled from "@emotion/styled";
 import { Stack } from "@mui/material";
-import React from "react";
+import { navigate } from "gatsby";
+import React, { useEffect, useState } from "react";
 import { PostNode } from "../../types/postTypes";
 import PostItem from "./PostItem";
+import PostPagination from "./PostPagination";
+
+const POST_PER_PAGE = 2;
 
 const PostStack = styled(Stack)`
   min-height: 50vh;
@@ -13,11 +17,36 @@ interface PostListProps {
 }
 
 const PostList = ({ posts }: PostListProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  useEffect(() => {
+    const qs = window.location.search;
+    const query = new URLSearchParams(qs);
+    const page = parseInt(query.get("page") || "1", 10);
+    setCurrentPage(page);
+  }, []);
+
+  const onChangePage = (_: React.ChangeEvent<unknown>, value: number) => {
+    setCurrentPage(value);
+    navigate(`?page=${value}`);
+  };
+
   return (
-    <PostStack spacing={2}>
-      {posts &&
-        posts?.map(({ node }) => <PostItem key={node.id} post={node} />)}
-    </PostStack>
+    <Stack spacing={2}>
+      <PostStack spacing={2}>
+        {posts &&
+          posts
+            ?.slice(1 - currentPage, POST_PER_PAGE * currentPage)
+            .map(({ node }) => <PostItem key={node.id} post={node} />)}
+      </PostStack>
+      {posts && (
+        <PostPagination
+          postTotalLength={posts.length}
+          currentPage={currentPage}
+          onChangePage={onChangePage}
+        />
+      )}
+    </Stack>
   );
 };
 
